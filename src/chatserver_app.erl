7f4c2494f8b4f9ddeddfa09a3c2e30c057ce2e37
Nick Bildeyko <bildeyko@gmail.com>
2015-01-10 19:06:15 +0300

    Added dispatch mechanism in the start func.

--- src/chatserver_app.erl
+++ src/chatserver_app.erl
@@ -9,7 +9,18 @@
 %% Application callbacks
 %% ===================================================================
 
+start() ->
+	application:start(cowboy),
+	application:start(chatserver).
+
 start(_StartType, _StartArgs) ->
+	Dispatch = cowboy_router:compile([
+		{'_', [{"/", index_handler, []}]}
+	]),
+	cowboy:start_http(http_listener, 100,
+		[{port, 8080}],
+		[{env, [{dispatch, Dispatch}]}]
+	),
     chatserver_sup:start_link().
 
 stop(_State) ->
