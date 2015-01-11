63df89b54dd9d4e9427753f27249abdec38911dd
Nick Bildeyko <bildeyko@gmail.com>
2015-01-11 14:58:25 +0300

    Deleted start/0 in chatserver_app.erl

--- src/chatserver_app.erl
+++ src/chatserver_app.erl
@@ -1,26 +1,25 @@
 -module(chatserver_app).
-
 -behaviour(application).
 
-%% Application callbacks
 -export([start/2, stop/1]).
 
-%% ===================================================================
-%% Application callbacks
-%% ===================================================================
-
-start() ->
-	application:start(cowboy),
-	application:start(chatserver).
+%start() ->
+%	application:start(cowboy),
+%	application:start(chatserver).
 
 start(_StartType, _StartArgs) ->
 	Dispatch = cowboy_router:compile([
-		{'_', [{"/", index_handler, []}]}
+		{'_', [
+			{"/", index_handler, []}
+		]}
 	]),
-	cowboy:start_http(http_listener, 100,
-		[{port, 8080}],
-		[{env, [{dispatch, Dispatch}]}]
-	),
+	% It's settings for openshift hosting
+	%{ok, _} = cowboy:start_http(http_listener, 100, [{ip,{127,10,206,129}}, {port, 8080}], [
+	{ok, _} = cowboy:start_http(http_listener, 100, [
+		{ip,{127,0,0,0}}, 
+		{port, 8081}], [
+		{env, [{dispatch, Dispatch}]}
+	]),
     chatserver_sup:start_link().
 
 stop(_State) ->
